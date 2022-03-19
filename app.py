95f04c31b1c54de63fa17faf417032a2999be952
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-03-19 20:31:58 +0530

    [Valarmathy]: Implemented if condition python code for practice purpose

--- app.py
+++ app.py
@@ -40,4 +40,3 @@ price = 25
 print(price >10 and price < 30)
 print(price >10 or price < 30)
 print(not price >10)
- 
---
+++ ifcondition.py
@@ -0,0 +1,9 @@
+temperature = 25
+
+if temperature >30:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature > 20: # greater tha 20 but less tha 30
+    print("It's a nice day")
+else:
+    print("It's cold")
