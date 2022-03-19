e64e9782ea00623f654fc0a58da85eacc35ecfb5
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-03-19 20:07:20 +0530

    [Valarmathy]: Implemented few python basic codes for practice purpose

---
+++ app.py
@@ -0,0 +1,43 @@
+print("Hello World")
+
+age= 20
+age = 30
+price = 19.95
+first_name ="Valar"
+is_online = False
+print(age)
+
+patient_name = "John Smith"
+age =20
+is_new_patient=True
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+birth_year = input("Enter your birth Year : ")
+age = 2022 - int(birth_year)
+print(age)
+
+course = 'Python for Beginners'
+print(course.upper())
+print(course.lower())
+print(course.find('n'))
+print(course.find('for'))
+print(course.replace('for', '4'))
+
+print('Python' in course)
+
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+x = 10
+x= x + 3
+x += 3
+
+price = 25
+print(price >10 and price < 30)
+print(price >10 or price < 30)
+print(not price >10)
+ 
---
+++ calculator.py
@@ -0,0 +1,5 @@
+first_num = input("First Number: " )
+second_num = input("Second Number: " )
+sum_num = float(first_num) + float(second_num)
+print("Sum : " +str(sum_num))
+
